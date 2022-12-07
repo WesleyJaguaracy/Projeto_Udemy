b9765172433602cd6b4654e20f496e011df9b956
DEV <DEV@PS-DESKTOP-CQI8>
2022-12-07 12:48:29 -0300

    Add project files.

---
+++ Projeto_Udemy/16.Aula.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Projeto_Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';
+            int n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria";
+
+            Console.WriteLine(completo);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+        }
+    }
+}

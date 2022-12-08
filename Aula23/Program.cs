4b5e02310735ee774c4e39aff6a9538133444bba
DEV <DEV@PS-DESKTOP-CQI8>
2022-12-08 18:06:57 -0300

    AtualizaçãoAula23

---
+++ Aula23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Você digitou: ");
+            Console.Write(n1);
+        }
+    }
+}
